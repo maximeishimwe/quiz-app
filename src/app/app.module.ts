f7f3d926653db44e0cc1c241587d9520d17b56d4
Maxime Ishimwe <mxmishimwe5@gmail.com>
2024-07-03 17:08:04 +0200

    initial routing, navbar, theme toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ThemeSwitcherComponent } from './components/theme-switcher/theme-switcher.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ThemeSwitcherComponent
   ],
   imports: [
     BrowserModule,
